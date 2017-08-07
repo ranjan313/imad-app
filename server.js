32d22881a91ae165d7c87ed960f2dbbdef59b480
ranjan313 <ranjan.sr7ei@gmail.com>
2017-08-07 18:44:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,14 @@ app.get('/', function (req, res) {
 });
 app.get('/article-one', function(req, res){
    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+   
 });
+
+app.get('/ui/article-one.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.css'));
+});
+
+
 app.get('/article-two', function(req, res){
     res.send('article two requested and will be served here');
 });
